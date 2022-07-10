efdb18081d5e3d33aa8145f630263fd9fff0574c
siddharthrajaraja <siddharthraja9849@gmail.com>
2022-07-10 12:38:55 +0530

    kafka_producer: add express server for kafka_producer

--- kafka_producer/app.ts
+++ kafka_producer/app.ts
@@ -1,3 +1,12 @@
 /**
  * Kafka Producer Microservice
  */
+ import express, { Express } from "express";
+ import loadConfigs from "load_configs";
+ 
+ const PORT = loadConfigs().KAFKA_PRODUCER_PORT 
+ const app: Express = express()
+ 
+ app.listen(PORT,()=>{
+     console.log(`KAFKA_PRODUCER_SERVICE at PORT ${PORT}`)
+ })
