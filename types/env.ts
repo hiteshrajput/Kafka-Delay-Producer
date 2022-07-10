253cc3deb9d5bc43254dabd6028a1bb21457837d
siddharthrajaraja <siddharthraja9849@gmail.com>
2022-07-10 22:06:09 +0530

    all: add MONGODB_URL and MONGODB_DATABASE in env

--- types/env.ts
+++ types/env.ts
@@ -1,10 +1,12 @@
-interface ENV  {
-    DELAY_API_SERVER_HOST: string,
-    DELAY_API_SERVER_PORT: number
-    KAFKA_PRODUCER_HOST: string,
-    KAFKA_PRODUCER_PORT: number,
-    REDIS_SUBSCRIBER_HOST:string,
-    REDIS_SUBSCRIBER_PORT: number,
-} 
+interface ENV {
+  DELAY_API_SERVER_HOST: string;
+  DELAY_API_SERVER_PORT: number;
+  KAFKA_PRODUCER_HOST: string;
+  KAFKA_PRODUCER_PORT: number;
+  REDIS_SUBSCRIBER_HOST: string;
+  REDIS_SUBSCRIBER_PORT: number;
+  MONGODB_URL: string;
+  MONGODB_DATABASE: string;
+}
 
-export default ENV
+export default ENV;
