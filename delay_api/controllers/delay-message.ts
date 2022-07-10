a7442349ffb9d0fe67c920df357115957d433a84
siddharthrajaraja <siddharthraja9849@gmail.com>
2022-07-10 22:09:30 +0530

    delay_api: update delayMessage controller with saveDelayMessage

--- delay_api/controllers/delay-message.ts
+++ delay_api/controllers/delay-message.ts
@@ -1,12 +1,20 @@
-import responsCode from "constants/response-code";
+import responseCode from "constants/response-code";
 import responseMessage from "constants/response-message";
+import saveDelayMessage from "delay_api/db/connection/mongo/query/save-delay-message";
 import { Request, Response } from "express";
 import { DelayMessage } from "types/delay-message";
 
-export default function delayMessage(req: Request, res: Response) {
-  const message: DelayMessage = req.body as DelayMessage;
-  console.log(typeof message.delayTime);
-  return res.status(responsCode.SUCCESS).json({
-    message: responseMessage.OK,
-  });
+export default async function delayMessage(req: Request, res: Response) {
+  try {
+    const message: DelayMessage = req.body as DelayMessage;
+
+    // insert delay message in mongodb
+    const data = await saveDelayMessage(message);
+
+    return res.status(responseCode.SUCCESS).json({
+      message: responseMessage.SUCCESSFULLY_INSERTED_MESSAGE,
+    });
+  } catch (error) {
+    return res.status(responseCode.BAD_REQUEST).send(error);
+  }
 }
