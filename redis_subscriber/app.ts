f7daedcb66a12945ac37be4e67ae5a495dc64c06
siddharthrajaraja <siddharthraja9849@gmail.com>
2022-07-10 12:46:44 +0530

    redis_subscriber: add redis_subscriber express server

--- redis_subscriber/app.ts
+++ redis_subscriber/app.ts
@@ -1,3 +1,12 @@
 /**
  * Redis subscriber Microservice
  */
+ import express, { Express } from "express";
+ import loadConfigs from "load_configs";
+ 
+ const PORT = loadConfigs().REDIS_SUBSCRIBER_PORT 
+ const app: Express = express()
+ 
+ app.listen(PORT,()=>{
+     console.log(`REDIS_SUBSCRIBER at PORT ${PORT}`)
+ })
