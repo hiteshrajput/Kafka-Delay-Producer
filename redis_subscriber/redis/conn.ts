a851994773275e20f02c0b2ae011d81d78c18a4e
siddharthrajaraja <siddharthraja9849@gmail.com>
2022-07-13 13:24:36 +0530

    redis_subscriber: add redisClient2 in conn

--- redis_subscriber/redis/conn.ts
+++ redis_subscriber/redis/conn.ts
@@ -4,8 +4,13 @@ const redisClient = createClient({
   url: "redis://localhost:6379",
 });
 
+const redisClient2 = createClient({
+  url: "redis://localhost:6379",
+});
+
 redisClient.connect();
+redisClient2.connect();
 
 console.log("REDIS CLIENT CREATED");
 
-export default redisClient;
+export { redisClient, redisClient2 };
