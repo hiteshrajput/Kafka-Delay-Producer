ce53e595967b678d6e932ba239cbefabd3d390a4
siddharthrajaraja <siddharthraja9849@gmail.com>
2022-07-13 21:03:55 +0530

    kafka_producer: add producer script in produceMessageToKafka to push message in kafka topic

--- kafka_producer/controller/produce-to-kafka.ts
+++ kafka_producer/controller/produce-to-kafka.ts
@@ -1,12 +1,30 @@
 import responseCode from "constants/response-code";
 import responseMessage from "constants/response-message";
 import { Request, Response } from "express";
+import { Producer, Message } from "kafkajs";
+import kafka from "kafka_producer/kafka/config";
 import { Payload } from "types/delay-message";
 
-export default function produceMessageToKafka(req: Request, res: Response) {
+export default async function produceMessageToKafka(
+  req: Request,
+  res: Response
+) {
   const payload: Payload = req.body;
   console.log(
     `Message received to KAFKA for TOPIC : ${payload.topic} is ${payload.message}`
   );
-  return res.status(responseCode.SUCCESS).send(responseMessage.OK);
+  const producer: Producer = kafka.producer();
+  await producer.connect();
+  const producerObj = {
+    topic: payload.topic,
+    messages: [
+      {
+        key: payload.topic,
+        value: payload.message,
+      },
+    ],
+  };
+  const kafkaResponse = await producer.send(producerObj);
+  console.log(`MESSAGE IN KAFKA DONE :-)`);
+  return res.status(responseCode.SUCCESS).send(kafkaResponse);
 }
